150dd6f54254048e83c9950e0e07f482b7f9b8a0
jai sharma <jai691666@gmail.com>
2026-09-20 14:40:33 +0530

    Connect db with server create post and get api and fetch data from database

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const app = require('./src/app')
+const db = require('./db')
+
+
+
+db.query('SELECT 1', (err, result)=>{
+  if(err){
+    console.log('DB connection failed', err)
+  } else {
+    console.log('DB connection successful')
+  }
+})
+
+
+
 
 app.listen(3000, ()=>{
   console.log('Server listen on 3000  port')
--- src/app.js
+++ src/app.js
@@ -1,24 +1,67 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+const db = require("../db");
 
-const notes = []
+// const notes = []
 
-app.post('/notes', (req , res)=>{
-    notes.push(req.body)
+// app.post('/notes', (req , res)=>{
+//     notes.push(req.body)
+//     res.status(200).json({
+//       success: true,
+//       message:'note add successfully'
+//     })
+// })
+
+// app.get('/notes', (req, res)=>{
+//   res.status(200).json({
+//      success: true,
+//       message:'note fetch successfully',
+//       data:notes
+//   })
+// })
+
+app.post("/api/notes", (req, res) => {
+  const { title, description } = req.body;
+
+  const sql = `INSERT INTO notes (title, description)
+    VALUES (?, ?)
+    `;
+
+    db.query(sql, [title, description],(err, result)=>{
+      if(err){
+        console.log(err);
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "note add successfully",
+        data: {
+          id: result.insertId,
+          title,
+          description
+        }
+      })
+    } )
+    
+});
+
+
+app.get('/api/notes', (req, res)=>{
+  const sql = `SELECT * FROM notes`;
+
+  db.query(sql, (err, result)=>{
+    if(err){
+      console.log(err);
+    }
+
     res.status(200).json({
       success: true,
-      message:'note add successfully'
-    })
-})
-
-app.get('/notes', (req, res)=>{
-  res.status(200).json({
-     success: true,
       message:'note fetch successfully',
-      data:notes
+      data:result
+    })
   })
 })
 
-module.exports= app
+module.exports = app;
